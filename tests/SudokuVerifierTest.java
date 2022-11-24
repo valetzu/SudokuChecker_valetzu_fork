7d907be143ddc58b69f20766b9c899051830a186
valetzu <valzerlazer@gmail.com>
2022-11-24 20:39:30 +0200

    Created boundary value tests for the sudokuVerifier.

--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -25,5 +25,66 @@ SudokuVerifier v = new SudokuVerifier();
 		
 	}
 	
+	@Test
+	public void testLowerBoundaryValuesInvalidFullSolution() {
+		//contains invalid lower boundary value
+		String test = "017369825632158947958724316825437169791586432346912758289643571573291684164875293";
+		int a = v.verify(test);
+		assertEquals("A cell in a Sudoku game can only store positive digits, i.e 1...9.", -1, a);
+	}
+	
+	@Test
+	public void testUpperBoundaryValuesInvalidFullSolution() {
+		//Upper boundary value (valid)
+		String test = "1023456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		int a = v.verify(test);
+		assertEquals("A cell in a Sudoku game can only store positive digits, i.e 1...9.", -1, a);
+	}
+	
+	@Test
+	public void testLowerBoundaryValueInvalidSingleDigitSolution() {
+		//Upper boundary value (valid)
+		String test = "0";
+		int a = v.verify(test);
+		assertEquals("A cell in a Sudoku game can only store positive digits, i.e 1...9.", -1, a);
+	}
+	
+	@Test
+	public void testSolutionWithInvalidNonIntegerChar() {
+		//Upper boundary value (valid)
+		String test = " 123123789912345678891234567789123456678912345567891234456789123345678912234567891";
+		int a = v.verify(test);
+		assertEquals("A cell in a Sudoku game can only store positive digits, i.e 1...9.", -1, a);
+	}
+	
+	@Test
+	public void testNegativeInteger() {
+		//Upper boundary value (valid)
+		String test = "-123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+		int a = v.verify(test);
+		assertEquals("A cell in a Sudoku game can only store positive digits, i.e 1...9.", -1, a);
+	}
+	
+	@Test
+	public void testInvalidSingleDigitSolution() {
+		//Upper boundary value (valid)
+		String test = "1";
+		int a = v.verify(test);
+		assertEquals("A cell in a Sudoku game can only store positive digits, i.e 1...9.", -1, a);
+	}
+	
+	@Test
+	public void testInvalidTwoDigitSolution() {
+		//Upper boundary value (valid)
+		String test = "10";
+		int a = v.verify(test);
+		assertEquals("A cell in a Sudoku game can only store positive digits, i.e 1...9.", -1, a);
+	}
+	
+	
+	
+	
+	
+	
 	
 }
